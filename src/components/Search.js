c986ad19b88cb9c98029096c239546d86e5411c9
rvthorve <rvthorve@gmail.com>
2021-12-12 21:35:28 -0500

    url

--- src/components/Search.js
+++ src/components/Search.js
@@ -56,7 +56,7 @@ function Search() {
 const submitHandler = async () => {
   setLoading(true)
   if(query!==null) {
-    let url = `http://3.87.211.154:9999/tweets?query=${query}`
+    let url = `/tweets?query=${query}`
     // let newsUrl = `https://newsapi.org/v2/everything?q=${query}&sortBy=relevancy&apiKey=7b68fb36cbd144b4bccab639e9247928`
     if(poiValue.length!==0) {
       console.log(poiValue)
@@ -78,6 +78,7 @@ const submitHandler = async () => {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods":'GET,PUT,POST,DELETE,PATCH,OPTIONS',
       },
+      baseUrl : 'http://3.87.211.154:9999/',
       url: url
     }
     // const newsRequest = {
